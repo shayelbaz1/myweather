440f10816de27007817dd86b9f3fa2e90f8f2417
shay <shaysha6@gmail.com>
2021-01-17 05:24:07 +0200

    fix api

--- src/app/services/weather-service/weather.service.ts
+++ src/app/services/weather-service/weather.service.ts
@@ -11,11 +11,17 @@ import { ToastrService } from 'ngx-toastr';
 import { environment } from 'src/environments/environment';
 import { utils } from '../utils.service';
 
+
+// const apiKey = 'ywdGKGqV43j2QCwAnwtZJWDTCXk1GA40'
+const apiKey = 'OQ87QXhI3w8c6js4YH4P6DAZPUUP8iA6'
+// const apiKey = '2Nj1GACFkAWToLTjCAOkdsVz5RtrsOym'
+// const apiKey = 'nm3Wjiu4zKHNtHswv4apdtf5j47VOpln'
+
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
-  private readonly apikey = environment.apiKey;
+  private readonly apikey = apiKey;
   isMetricTempUnit = true;
   defaultCity$: Observable<City>;
   favorites$: Observable<City[]>;
